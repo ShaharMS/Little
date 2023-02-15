71ca294533d751ee199d33a83cf77d65a0cfd8a9
Shahar Marcus <88977041+ShaharMS@users.noreply.github.com>
2023-02-15 22:41:52 +0200

    merge blocks

--- src/refactored_little/parser/Parser.hx
+++ src/refactored_little/parser/Parser.hx
@@ -1,7 +1,51 @@
-// package refactored_little.parser;
+package refactored_little.parser;
 
-// import refactored_little.lexer.Tokens.LexerTokens;
+import refactored_little.parser.Tokens.ParserTokens;
+import refactored_little.lexer.Tokens.LexerTokens;
 
-// class Parser {
-//     public static function parse(tokens:Array<LexerTokens>):Array<>
-// }
+class Parser {
+    public static function parse(lexerTokens:Array<LexerTokens>):Array<ParserTokens> {
+        var tokens:Array<ParserTokens> = [];
+
+        var i = 0;
+        while (i < lexerTokens.length) {
+            
+            i++;
+        }
+
+
+        return tokens;
+    }
+
+    public static function mergeBlocks(pre:Array<ParserTokens>):Array<ParserTokens> {
+        var post:Array<ParserTokens> = [];
+
+        var i = 0;
+        while (i < pre.length) {
+            var token = pre[i];
+            switch token {
+                case Sign("{"): {
+                    var blockBody:Array<ParserTokens> = [];
+                    var blockStack = 1; // Open and close the block on the correct curly bracket
+                    while (i + 1 < pre.length) {
+                        var lookahead = pre[i + 1];
+                        if (Type.enumEq(lookahead, Sign("{"))) {
+                            blockStack++;
+                            blockBody.push(lookahead);
+                        } else if (Type.enumEq(lookahead, Sign("}"))) {
+                            blockStack--;
+                            if (blockStack == 0) break;
+                            blockBody.push(lookahead);
+                        } else blockBody.push(lookahead);
+                        i++;
+                    }
+                    post.push(Block(mergeBlocks(blockBody), null)); // The check performed above includes unmerged blocks inside the outer block. These unmerged blocks should be merged
+                }
+                case _: post.push(token);
+            }
+            i++;
+        }
+
+        return post;
+    }
+}
---
+++ src/refactored_little/parser/Tokens.hx
@@ -0,0 +1,27 @@
+package refactored_little.parser;
+
+enum ParserTokens {
+
+    SetLine(line:Int);
+
+    Define(name:String, value:ParserTokens, type:String);
+    Action(name:String, params:Array<ParserTokens>, body:ParserTokens, type:String);
+    Condition(name:String, exp:ParserTokens, body:ParserTokens, type:String);
+
+    Read(name:String);
+    Write(assignees:Array<String>, value:ParserTokens);
+
+    Expression(parts:Array<ParserTokens>, type:String);
+    Block(body:Array<ParserTokens>, type:String);
+
+    Parameter(name:String, value:ParserTokens, type:String);
+
+    Sign(sign:String);
+    Number(num:String);
+    Decimal(num:String);
+    Characters(string:String);
+
+    NullValue;
+    TrueValue;
+    FalseValue;
+}
