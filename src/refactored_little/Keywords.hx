052037825120259bd7694f97c9d224ea30c7bd4c
ShaharMS <shaharmsecond@gmail.com>
2023-02-26 15:23:57 +0200

    RAISE_ERROR_FUNCTION_NAME: stop => error

--- src/refactored_little/Keywords.hx
+++ src/refactored_little/Keywords.hx
@@ -18,7 +18,7 @@ class Keywords {
     public static var MAIN_MODULE_NAME:String = "Main";
     public static var REGISTERED_MODULE_NAME:String = "Registered";
     public static var PRINT_FUNCTION_NAME:String = "print";
-    public static var RAISE_ERROR_FUNCTION_NAME:String = "stop";
+    public static var RAISE_ERROR_FUNCTION_NAME:String = "error";
 
     /**
     	No need to ever change this, this is a parser-only feature
