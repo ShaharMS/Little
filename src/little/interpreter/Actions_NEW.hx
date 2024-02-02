a263e25276f95a7bc4a6f6b054c2383163d59e2d
ShaharMS <shaharmsecond@gmail.com>
2024-02-02 21:29:35 +0200

    progress

---
+++ src/little/interpreter/Actions_NEW.hx
@@ -0,0 +1,11 @@
+package little.interpreter;
+
+import little.interpreter.Tokens.InterpTokens;
+
+class Actions_NEW {
+    
+    public static function run(tokens:Array<InterpTokens>):InterpTokens {
+        return null;
+    }
+
+}
